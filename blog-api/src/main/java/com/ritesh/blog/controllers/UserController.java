2fb6a547eaab72eeb793d048bfeb2abdc2830880
rhansalia <rhansalia@STAUNCHSYS-26>
2023-04-28 18:08:24 +0530

    third commint

--- blog-api/src/main/java/com/ritesh/blog/controllers/UserController.java
+++ blog-api/src/main/java/com/ritesh/blog/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.ritesh.blog.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +25,34 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping("/getUsers")
+	public ResponseEntity<List<UserDto>> getAllUser(){
+		return ResponseEntity.ok(this.userService.getAllUser());
+	}
+	
+	
+	@GetMapping("/getUser/{userId}")
+	public ResponseEntity<UserDto> getUser(@PathVariable int userId){
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
+	
+	
 	@PostMapping("/createUser")
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
+	
+	
+	@PutMapping("/updateUser/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable Integer userId){
+		UserDto updatedUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	@DeleteMapping("/deleteUser/{userId}")
+	public ResponseEntity<?> deleteUser(@PathVariable int userId){
+		this.userService.deleteUser(userId);
+		return new ResponseEntity(Map.of("message","User deleted Successfully"),HttpStatus.OK);
 	}
 }
