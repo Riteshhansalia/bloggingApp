ce631002cfbde84346d47948099deead4a0e3297
rhansalia <rhansalia@STAUNCHSYS-26>
2023-05-03 18:23:32 +0530

    create post entity

--- blog-api/src/main/java/com/ritesh/blog/entities/Category.java
+++ blog-api/src/main/java/com/ritesh/blog/entities/Category.java
@@ -1,9 +1,15 @@
 package com.ritesh.blog.entities;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -31,5 +37,8 @@ public class Category {
 	@Size(max = 10,message = "Maximum size of discription is 10!")
 	@Column(name="description")
 	private String categoryDescription;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 	
 }
---
+++ blog-api/src/main/java/com/ritesh/blog/entities/Post.java
@@ -0,0 +1,46 @@
+package com.ritesh.blog.entities;
+
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name ="post")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int postId;
+	
+	@Column(name = "post_title", length = 100, nullable = false)
+	private String postTitle;
+	
+	@Column(name="post_content", length = 10000)
+	private String postContent;
+	
+	private String imageName;
+	
+	private Date addedDate;
+	
+	@ManyToOne
+	@JoinColumn(name="category_id")
+	private Category category;
+	
+	@ManyToOne
+	private User user;
+	
+	
+}
